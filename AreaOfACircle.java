259f15f219a75238834ba2ae8184dc81ffdc419d
erobinofficial <erobin8778@gmail.com>
2023-02-01 18:55:51 +0600

    ok

--- AreaOfACircle.java
+++ AreaOfACircle.java
@@ -1,9 +1,6 @@
-import java.util.Scanner;
 public class AreaOfACircle {
     public static void main(String[] args) {
-        System.out.print("Enter radius of circle: ");
-        Scanner input = new Scanner(System.in);
-        Double radius = input.nextDouble();
+        int radius = 20;
         Double area = Math.PI * radius * radius;        
         System.out.format("Area: %.2f", area);
     }
